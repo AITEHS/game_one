c6db895a7345ae9277249a75576e0e582f4a93b6
Aitehs <turetskiy5002@gmail.com>
2021-12-15 01:04:42 +0100

    Jump

--- witcher.py
+++ witcher.py
@@ -1,37 +1,77 @@
-import sys, pygame
+import pygame, time, math, random, sys
+from pygame.locals import *
+
+background = pygame.Surface((640, 400))
+background.fill((30, 90, 120))
+
+W, H = 640, 400
+HW, HH = W / 2, H / 2
+AREA = W * H
+FPS = 60
+bg_x = 0
+
 pygame.init()
-
-size = width, height = 1920,1080
-speed = [1,1]
-black = 0,0,0
-
-screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((W,H))
 
 
-hero = pygame.image.load("images/hero.png")
-herorect = hero.get_rect()
+class Mario():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # isJump and jumpCount should be attributes of Mario.
+        self.isJump = False
+        self.jumpCount = 10
+
+    def draw(self):
+        self.player_rect = Rect((self.x, self.y, 40, 40)) 
+        pygame.draw.rect(screen, (255,0,0),self.player_rect)
+
+    def move(self):
+        global bg_x
+        if pressed_keys[K_d] and self.x< screen.get_width()-5 - self.player_rect.width:
+            self.x += 5
+                
+        if pressed_keys[K_a] and self.x > 5:
+            self.x -= 5
+            
+
+
+    def jump(self):
+        # Check if mario is jumping and then execute the
+        # jumping code.
+        if self.isJump:
+            if self.jumpCount >= -10:
+                neg = 1
+                if self.jumpCount < 0:
+                    neg = -1
+                self.y -= self.jumpCount**2 * 0.25 * neg
+                if pressed_keys[K_d] and pressed_keys[K_LSHIFT] and self.x< screen.get_width()-5 - self.player_rect.width:
+                    self.x += 10 
+                if pressed_keys[K_a] and pressed_keys[K_LSHIFT] and self.x>5:
+                    self.x -= 10 
+                self.jumpCount -= 1
+            else:
+                self.isJump = False
+                self.jumpCount = 10
+
+
+mario = Mario(50, 270)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # Start to jump by setting isJump to True.
+                mario.isJump = True
 
-
-    if pygame.key.get_pressed()[pygame.K_a]:
-        herorect.x -= 5
-    if pygame.key.get_pressed()[pygame.K_d]: 
-        herorect.x += 5
-    if pygame.key.get_pressed()[pygame.K_s]:
-        herorect.y += 5
-    if pygame.key.get_pressed()[pygame.K_w]:    
-        herorect.y -= 5
-
-
-    #herorect = herorect.move(speed)
-    if herorect.left < 0 or herorect.right > width:
-        herorect = herorect.move([0,0])
-   # if herorect.top < 0 or herorect.bottom > height:
-    #    speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(hero, herorect)
-    pygame.display.flip()
+    clock.tick(FPS)
+    pressed_keys = pygame.key.get_pressed()
+    screen.blit(background, (bg_x,0))
+    mario.move()
+    mario.draw()
+    mario.jump()
+    pygame.display.update()
